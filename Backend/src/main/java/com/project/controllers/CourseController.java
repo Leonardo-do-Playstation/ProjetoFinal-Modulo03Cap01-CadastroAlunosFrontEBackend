a53e04b96e8f090cd3b894cc374f4ff11d3ddd95
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-03-06 17:25:33 -0300

    Altera versão do Java e cria metodos GetAll, GetById and createStudent na classe studentController

--- Backend/src/main/java/com/project/controllers/CourseController.java
+++ Backend/src/main/java/com/project/controllers/CourseController.java
@@ -1,3 +1,4 @@
+package com.project.controllers;
 public class CourseController {
     
 }
--- Backend/src/main/java/com/project/controllers/StudentController.java
+++ Backend/src/main/java/com/project/controllers/StudentController.java
@@ -1,3 +1,52 @@
+
+package com.project.controllers;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.entities.Student;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
-    
+
+    public List<Student> students = new ArrayList<>();
+
+    public StudentController() {
+
+        students.add(new Student(1, "Jonas Silva", "jonas.silva@email.com", "11987654321", 101, 1));
+        students.add(new Student(2, "Maria Souza", "maria.souza@email.com", "11976543210", 102, 3));
+        students.add(new Student(3, "João Pereira", "joao.pereira@email.com", "11965432109", 101, 5));
+        students.add(new Student(4, "Ana Costa", "ana.costa@email.com", "11954321098", 103, 2));
+        students.add(new Student(5, "Lucas Martins", "lucas.martins@email.com", "11943210987", 102, 4));
+
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return students.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        students.add(student);
+        return ResponseEntity.status(HttpStatus.CREATED).body(student);
+    }
+
 }
