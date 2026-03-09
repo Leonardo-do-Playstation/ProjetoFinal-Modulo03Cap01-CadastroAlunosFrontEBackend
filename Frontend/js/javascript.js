2e7386c3eda1b0f4f3df350fae5be758d36daffe
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-03-09 16:11:39 -0300

    Carrega students e courses da API e refatora a funcao de salvar student

--- Frontend/js/javascript.js
+++ Frontend/js/javascript.js
@@ -1,14 +1,4 @@
-var students = [
-    { id: 1, name: "Pedro Antonio", email: "pedro@abutua.com", phone: "(15) 9999-9999", curse: 2, turn: 2 },
-];
-
-var curses = [
-    { id: 1, name: "Java" },
-    { id: 2, name: "Angular" },
-    { id: 3, name: "C#" },
-    { id: 4, name: "Python" },
-    { id: 5, name: "JavaScript" },
-];
+var curses = [];
 
 var turns = [
     { id: 1, name: "Manhã" },
@@ -16,28 +6,73 @@ var turns = [
     { id: 3, name: "Noite" },
 ];
 
+loadCourses();
 loadStudents();
 
 function loadStudents() {
-    for (let stud of students) {
-        addNewRow(stud);
-    }
+
+fetch("http://localhost:8080/students")
+    .then(response => response.json())
+    .then(data => {
+
+        var table = document.getElementById("studentsTable");
+        table.innerHTML = "";
+        for (let stud of data) {
+            addNewRow(stud);
+        }
+
+    });
 }
 
+function loadCourses(){
+
+fetch("http://localhost:8080/courses")
+    .then(response => response.json())
+    .then(data => {
+
+        curses = data;
+
+        var select = document.getElementById("inputCourse");
+        select.innerHTML = "";
+
+        for(let course of data){
+
+            var option = document.createElement("option");
+
+            option.value = course.id;
+            option.text = course.name;
+
+            select.appendChild(option);
+
+        }
+
+    });
+
+}
 
 function save() {
     var stud = {
-        id: students.length + 1,
-        name: document.getElementById("inputName").value,
+         name: document.getElementById("inputName").value,
         email: document.getElementById("inputEmail").value,
         phone: document.getElementById("inputPhone").value,
-        curse: document.getElementById("inputCurse").value,
-        turn: document.querySelector('input[name="gridRadios"]:checked').value
+        idCurso: parseInt(document.getElementById("inputCourse").value),
+        period: parseInt(document.querySelector('input[name="gridRadios"]:checked').value)
     };
 
-    addNewRow(stud);
-    students.push(stud);
-    document.getElementById("studentForm").reset();
+    fetch("http://localhost:8080/students", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(stud)
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            loadStudents();
+            document.getElementById("studentForm").reset();
+
+        });
 }
 
 
@@ -45,30 +80,24 @@ function addNewRow(stud) {
     var table = document.getElementById("studentsTable");
     var newRow = table.insertRow();
 
-    var idNode = document.createTextNode(stud.id);
-    newRow.insertCell().appendChild(idNode);
+    newRow.insertCell().appendChild(document.createTextNode(stud.id));
+    newRow.insertCell().appendChild(document.createTextNode(stud.name));
 
-    var nameNode = document.createTextNode(stud.name);
-    newRow.insertCell().appendChild(nameNode);
-
-    var emailNode = document.createTextNode(stud.email);
     var cell = newRow.insertCell();
     cell.className = "d-none d-md-table-cell";
-    cell.appendChild(emailNode);
+    cell.appendChild(document.createTextNode(stud.email));
 
-    var phoneNode = document.createTextNode(stud.phone);
     var cell2 = newRow.insertCell();
     cell2.className = "d-none d-md-table-cell";
-    cell2.appendChild(phoneNode);
+    cell2.appendChild(document.createTextNode(stud.phone));
 
-    var curseNode = document.createTextNode(curses[stud.curse - 1].name);
+    var course = curses.find(c => c.id == stud.idCurso);
+
     var cell3 = newRow.insertCell();
     cell3.className = "d-none d-md-table-cell";
-    cell3.appendChild(curseNode);
+    cell3.appendChild(document.createTextNode(course.name));
 
-    var turnNode = document.createTextNode(turns[stud.turn - 1].name);
     var cell4 = newRow.insertCell();
     cell4.className = "d-none d-md-table-cell";
-    cell4.appendChild(turnNode);
-
+    cell4.appendChild(document.createTextNode(turns[stud.period - 1].name));
 }
