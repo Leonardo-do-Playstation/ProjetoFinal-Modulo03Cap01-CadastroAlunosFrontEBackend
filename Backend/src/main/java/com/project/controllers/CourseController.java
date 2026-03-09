17219644737285d96ddd0e05b35c433b2a9da358
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-03-09 13:35:14 -0300

    Cria metodo getAll na classe CourseController

--- Backend/src/main/java/com/project/controllers/CourseController.java
+++ Backend/src/main/java/com/project/controllers/CourseController.java
@@ -1,4 +1,31 @@
 package com.project.controllers;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.entities.Course;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
     
+    public List<Course> courses = new ArrayList<>();
+
+    public CourseController() {
+        courses.add(new Course(101, "Banco de dados"));
+        courses.add(new Course(102, "Programação Orientada a Objetos"));
+        courses.add(new Course(103, "Algoritmos"));
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses(){
+        return courses;
+    }
+
 }
