f25d5bf64df7870a5c5ddc410e1c01d253c64baf
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-03-09 15:48:32 -0300

    Troca tipo de alguns atributos no Student

--- Backend/src/main/java/com/project/entities/Student.java
+++ Backend/src/main/java/com/project/entities/Student.java
@@ -2,18 +2,18 @@ package com.project.entities;
 
 public class Student {
     
-    private int id;
+    private Integer id;
     private String name;
     private String email;
     private String phone;
-    private int idCurso;
-    private int period;
+    private Integer idCurso;
+    private Integer period;
 
     public Student() {
 
     }
 
-    public Student(int id, String name, String email, String phone, int idCurso, int period) {
+    public Student(Integer id, String name, String email, String phone, Integer idCurso, Integer period) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -22,11 +22,11 @@ public class Student {
         this.period = period;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -54,19 +54,19 @@ public class Student {
         this.phone = phone;
     }
 
-    public int getIdCurso() {
+    public Integer getIdCurso() {
         return idCurso;
     }
 
-    public void setIdCurso(int idCurso) {
+    public void setIdCurso(Integer idCurso) {
         this.idCurso = idCurso;
     }
 
-    public int getPeriod() {
+    public Integer getPeriod() {
         return period;
     }
 
-    public void setPeriod(int period) {
+    public void setPeriod(Integer period) {
         this.period = period;
     }
 
